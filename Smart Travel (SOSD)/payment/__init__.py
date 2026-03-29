2de43f7e1d8bc35a9746d15adc541abafe77d826
GenericDreamers <163614313+GenericDreamers@users.noreply.github.com>
2026-03-29 23:10:44 +0700

    Revert "more map improvements"
    This reverts commit 5c471c45cba611c176766bb189e6b81ee1bec54a.

--- Smart Travel (SOSD)/payment/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required, get_jwt_identity
-from db import query_db
-payment_bp = Blueprint('payment', __name__, url_prefix='/api/payment')
-@payment_bp.route("/process", methods=["POST"])
-def process():
-    # Placeholder for payment processing logic
-    return {"msg": "Payment processed successfully"}, 200

8ea40acc40e858afa4d08fc874a907376f173941
Son-eng <buihongson2804@gmail.com>
2026-04-05 18:30:03 +0700

    ...

--- main.py
+++ main.py
@@ -38,6 +38,9 @@ app.register_blueprint(analytics_bp)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+@app.route('/booking')
+def booking_page():
+    return render_template('booking.html')
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
+
